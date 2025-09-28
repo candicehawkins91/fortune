a745fe76c9d1668897587869089d7c3ac01272f9
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-27 20:09:38 -0500

    tried my js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function fortuneTeller(fName,age){
     if (fName.lenght >= 7) console.log("you will marry late in life")
-} else if (fName.lenght > 5 & <7) {
-    console.log("you will encounter a once and a life time opportunity")
-}   else {
-    {console.log("you will marry within a year")
+} 
+    else if (fName.lenght > 5 & <7) console.log("you will encounter a once and a life time opportunity")
+{
+    else 
+        console.log("you will marry within a year")
+}   
 
     }
 }
