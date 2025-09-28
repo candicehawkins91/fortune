0004aad41fbdfdae8849d4c409df6eb5500fab6b
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-27 20:00:05 -0500

    added some js

---
+++ script.js
@@ -0,0 +1,9 @@
+function fortuneTeller(fName,age){
+    if (fName.lenght >= 7) console.log("you will marry late in life")
+} else if (fName.lenght > 5 & <7) {
+    console.log("you will encounter a once and a life time opportunity")
+}   else {
+    {console.log("you will marry within a year")
+
+    }
+}
